dc5161ef0cfa791d0def44de4f43ed67e37e1272
Goutamsahu23 <goutamsahu602@gmail.com>
2023-12-23 00:26:22 +0530

    modification

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT=process.env.PORT || 4000
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:3000', // Replace with your frontend's origin
+   
     credentials: true,
   }));
 app.use(cookieParser());
